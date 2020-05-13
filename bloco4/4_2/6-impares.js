a4ffc1846ce3d3516b3829f56cf0e29dac54f70e
rfreitasbatista <rfreitasbatista@gmail.com>
2020-05-13 18:15:46 -0300

    Exercícios solucionados

--- bloco4/4_2/6-impares.js
+++ bloco4/4_2/6-impares.js
@@ -1,8 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior=0;
+let maior=[];
 for (let i=0; i<numbers.length;i++){
-   if (numbers[i]>maior) {
-     maior=numbers[i];
+   if (numbers[i]%2!==0) {
+     maior.push(numbers[i]);
    }
 }
-console.log(maior);
+console.log(maior.length);
