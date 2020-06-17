9ac6c9fae0fe7f6cb3fa3e54677ecd2b1191368e
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-17 20:34:35 -0300

    Exercicios bloco 8.2 feitos

---
+++ bloco8/8_2/aula.js
@@ -0,0 +1,101 @@
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 }
+    ]
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: '59' },
+      { name: 'Português', nota: '80' },
+      { name: 'Química', nota: '78' },
+      { name: 'Biologia', nota: '92' }
+    ]
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '76' },
+      { name: 'Português', nota: '90' },
+      { name: 'Química', nota: '70' },
+      { name: 'Biologia', nota: '80' }
+    ]
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '91' },
+      { name: 'Português', nota: '85' },
+      { name: 'Química', nota: '92' },
+      { name: 'Biologia', nota: '90' }
+    ]
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '70' },
+      { name: 'Português', nota: '70' },
+      { name: 'Química', nota: '60' },
+      { name: 'Biologia', nota: '50' }
+    ]
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: '80' },
+      { name: 'Português', nota: '82' },
+      { name: 'Química', nota: '79' },
+      { name: 'Biologia', nota: '75' }
+    ]
+  },
+]
+
+const reportStatus = (name, students) => {
+  const student = students.find((student) => student.nome === name);
+  return student.materias.map((materia) => (
+    `${materia.name} ${(materia.nota >= 60) ? 'Aprovado' : 'Reprovado'}`
+  ));
+};
+
+console.log(reportStatus('Natalia', estudantes));
+
+
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumEvens = (result, number) => ( number % 2 === 0) ? result += number: result;
+
+console.log(numbers.reduce(sumEvens))
+
+const getBestClass = (bestClass, actual) => (actual.nota > bestClass.nota) ? actual : bestClass;
+const allSubj = (students) => {
+  return students.map((aluno) => {
+  return {
+    name: aluno.nome,
+    materia: aluno.materias.reduce(getBestClass).name
+  }; 
+})
+};
+console.log(allSubj(estudantes))
