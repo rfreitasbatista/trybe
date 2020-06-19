2ee11048585bfedd81eb06a347cd0e316c775d67
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-19 08:57:46 -0300

    Bloco 8.3 feito

---
+++ bloco8/8_3/exercicios2.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+
+// escreva sum abaixo
+
+const sum = (...a) => {
+  let soma = 0
+  for(let index of a) {
+    soma += index
+  }
+  return soma;
+}
+
+assert.equal(sum(), 0)
+assert.equal(sum(1), 1)
+assert.equal(sum(1, 2), 3)
+assert.equal(sum(1, 2, 3), 6)
+assert.equal(sum(1, 2, 3, 4), 10)
+    
