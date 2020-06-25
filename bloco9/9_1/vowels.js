f6321bfd8bae1b533c4432dd1d682f5428df9866
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-25 14:28:02 -0300

    vowels feito

--- bloco9/9_1/vowels.js
+++ bloco9/9_1/vowels.js
@@ -1,7 +1,5 @@
 function vowels(str) {
 const vow = ['a', 'e', 'i', 'o', 'u']
-
 return str.toLowerCase().split('').reduce(((total, letra) => (vow.includes(letra) === true) ? total += 1: total), 0)
 }
-console.log(vowels('ovo'))
 module.exports = vowels;
