8230696992654b9e138a77865699c48ac0588392
rfreitasbatista <rfreitasbatista@gmail.com>
2020-06-25 14:05:18 -0300

    Exercicios jest feitos

--- bloco9/9_1/test/remake8_3.test.js
+++ bloco9/9_1/test/remake8_3.test.js
@@ -6,6 +6,9 @@ const remake5 = require('../../../bloco7/7_3/exercicio5')
 const remake6 = require('../../../bloco7/7_3/exercicio6')
 const remake7 = require('../../../bloco7/7_3/exercicio7')
 const remake8 = require('../../../bloco7/7_3/exercicio8')
+const remake9 = require('../../../bloco7/7_3/exercicio9')
+const remake10 = require('../../../bloco7/7_3/exercicio10')
+const remake11 = require('../../../bloco7/7_3/exercicio11')
 
 describe('Exercises 1 of block 7_3', () => {
     test('Sum of parameters', () => {
@@ -43,11 +46,42 @@ describe('Exercises 5 of block 7_3', () => {
 });
 
 describe('Exercises 6 of block 7_3', () => {
-    test('Remove number from array', () => {
+    test('Fizzbuzz', () => {
         expect(remake6(15)).toEqual("fizzbuzz");
         expect(remake6(9)).toEqual('fizz');
         expect(remake6(50)).toEqual('buzz');
         expect(remake6(7)).toEqual(7);
         expect(remake6('olá')).toEqual(false);
+    });
+});
+
+describe('Exercises 7 of block 7_3', () => {
+    test('Test if it is a string', () => {
+        expect(typeof(remake7)).toEqual("string");
+    });
+});
+
+describe('Exercises 8 of block 7_3', () => {
+    test('Test if it is a function', () => {
+        expect(typeof(remake8)).toEqual("function");
+    });
+});
+
+describe('Exercises 9 of block 7_3', () => {
+    test('returns the opposite', () => {
+        expect(remake9(false)).toBeTruthy(); 
+    });
+});
+
+describe('Exercises 10 of block 7_3', () => {
+    test('test if object its equal or not', () => {
+        expect(remake10.obj1).toEqual(remake10.obj2);
+        expect(remake10.obj2).not.toEqual(remake10.obj3); 
+    });
+});
+
+describe('Exercises 11 of block 7_3', () => {
+    test('test if bigger than', () => {
+        expect(remake11(31, 18)).toBeTruthy();
     });
 });
