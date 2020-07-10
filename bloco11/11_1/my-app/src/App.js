1d8cfac7c379029b14e8e08a3d0c58b973a9e44a
rfreitasbatista <rfreitasbatista@gmail.com>
2020-07-10 16:00:40 -0300

    POkedex e softskills

--- bloco11/11_1/my-app/src/App.js
+++ bloco11/11_1/my-app/src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const tasks = ['arrumar casa', 'comprar comida', 'limpar a sala', 'lavar roupa'];
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+    );
+  }
+const lis = tasks.map ( element => task(element));
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {lis}
     </div>
   );
 }
